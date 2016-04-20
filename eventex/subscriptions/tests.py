9ded1f0d0d5e34284f60a1ee1d522d89345c936f
Alcivan <alcivancosta@msn.com>
2016-04-19 23:32:15 -0300

    All your base are belong to us.

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -18,6 +18,10 @@ class SubscribeTest(TestCase):
     def test_html(self):
         'Html must contain input controls.'
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 5)
+        self.assertContains(self.resp, '<input', 6)
         self.assertContains(self.resp, 'type="text"', 4)
         self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        'Html must contain csrf token.'
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
