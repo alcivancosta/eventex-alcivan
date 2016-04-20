3c1a66bcff3da8f004398c21180bafcabcdbb8da
Alcivan <alcivancosta@msn.com>
2016-04-20 16:54:54 -0300

    All your base are belong to us.

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
@@ -13,6 +15,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('eventex.core.urls', namespace='core')),
 )
