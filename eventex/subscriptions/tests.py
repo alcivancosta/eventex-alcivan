202005f3ea79338aabd16c84d277c29f5eaaca23
Alcivan <alcivancosta@msn.com>
2016-04-19 23:02:21 -0300

    All your base are belong to us.

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -6,7 +6,10 @@ class SubscribeTest(TestCase):
     def setUp(self):
         self.resp = self.client.get('/inscricao/')
 
-
     def test_get(self):
         'GET /inscricao/ must return status code 200.'
         self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Response shoud be a rendered template.'
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
