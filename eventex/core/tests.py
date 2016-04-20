521483bf3f21d3138556ed7beabc1c9a526f829b
Alcivan <alcivancosta@msn.com>
2016-04-19 22:29:55 -0300

    All your base are belong to us.

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -3,7 +3,13 @@ from django.test import TestCase
 
 
 class HomepageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
     def test_get(self):
         'GET / must return status code 200.'
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Homepage must use template index.html.'
+        self.assertTemplateUsed(self.resp, 'index.html')
