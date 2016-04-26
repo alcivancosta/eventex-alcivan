12561efcdd56a6dc7f57f0f9ebb3cdf81267f2ec
Alcivan <alcivancosta@msn.com>
2016-04-26 17:18:10 -0300

    All your base are belong to us.

--- eventex/settings.py
+++ eventex/settings.py
@@ -112,10 +112,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
     'eventex.core',
     'eventex.subscriptions',
 )
