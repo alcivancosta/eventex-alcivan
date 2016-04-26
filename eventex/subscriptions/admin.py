55bcc6eda77f74de9787ea77a8732799fad0c6b9
Alcivan <alcivancosta@msn.com>
2016-04-26 16:48:24 -0300

    All your base are belong to us.

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,11 +1,16 @@
 # coding: utf-8
+from django.utils.datetime_safe import datetime
 from django.contrib import admin
 from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cpf', 'email','phone', 'created_at')
+    list_display = ('name', 'cpf', 'email','phone', 'created_at',
+                    'subscribed_today')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'cpf', 'email', 'phone', 'created_at')
+
+    def subscribed_today(self, obj):
+        return obj.created_at.date() == datetime.today().date()
 
 admin.site.register(Subscription, SubscriptionAdmin)
