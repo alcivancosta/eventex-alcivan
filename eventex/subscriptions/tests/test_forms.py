d624ae617e3472c4caf69ec653f3734f7db662a4
Alcivan <alcivancosta@msn.com>
2016-04-29 16:11:31 -0300

    All your base are belong to us.

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -7,3 +7,21 @@ class SubscriptionFormTest(TestCase):
         'Form must have 4 fields.'
         form = SubscriptionForm()
         self.assertItemsEqual(['name', 'cpf', 'email', 'phone'], form.fields)
+
+    def test_cpf_is_digit(self):
+        'CPF must only accept digits.'
+        data = dict(name='Henrique Bastos', email='henrique@bastos.net',
+                     cpf='12345678901', phone='21-96186180')
+        data.update({'cpf': 'ABCD5678901'})
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+    def test_cpf_has_11_digits(self):
+        'CPF must have 11 digits.'
+        data = dict(name='Henrique Bastos', email='henrique@bastos.net',
+                    cpf='12345678901', phone='21-96186180')
+        data.update({'cpf': '1234'})
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        self.assertItemsEqual(['cpf'], form.errors)
