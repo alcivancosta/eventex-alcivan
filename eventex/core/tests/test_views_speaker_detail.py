3e91adf37b48f8c39e8fcc606956978bf2c8652a
Alcivan <alcivancosta@msn.com>
2016-05-10 15:08:49 -0300

    All your base are belong to us.

---
+++ eventex/core/tests/test_views_speaker_detail.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from django.test import TestCase
+from django.core.urlresolvers import reverse as r
+
+
+class SpeakerDetailTest(TestCase):
+    def setUp(self):
+        url = r('core:speaker_detail', kwargs={'slug': 'henrique-bastos'})
+        self.resp = self.client.get(url)
+
+    def test_get(self):
+        'GET should result in 200.'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Template should be core/speaker_detail.html'
+        self.assertTemplateUsed(self.resp, 'core/speaker_detail.html')
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -4,3 +4,7 @@ from django.views.generic.simple import direct_to_template
 
 def homepage(request):
     return direct_to_template(request, template='index.html')
+
+
+def speaker_detail(request, slug):
+    return direct_to_template(request, 'core/speaker_detail.html')
